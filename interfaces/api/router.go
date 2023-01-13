6c721d7d489741dace675eb23805458b04862f2d
Achmad Ali Baidlowi <achmadali.dev@gmail.com>
2023-01-13 09:44:22 +0700

    docs: add api spec documentation (#3)
    * docs: add swagger ui
    
    * docs: add swagger resource and endpoint
    
    Co-authored-by: Achmad Ali Baidlowi <ali.baidlowi@majoo.id>

--- interfaces/api/router.go
+++ interfaces/api/router.go
@@ -4,18 +4,19 @@ import (
 	"net/http"
 
 	"github.com/achmadAlli/go-simple-boilerplate/interfaces/api/utils"
+	v1 "github.com/achmadAlli/go-simple-boilerplate/interfaces/api/v1"
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterRoute(e *echo.Echo) {
 	api := e.Group("/api")
-	v1 := api.Group("/v1")
+	gv1 := api.Group("/v1")
 
 	api.GET("/check", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, utils.NewResponse(nil, "server is up"))
 	})
 
-	v1.GET("/check", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, utils.NewResponse(nil, "api v1 is up"))
-	})
+	api.Static("/docs", "docs/swagger-ui")
+
+	v1.RegisterV1(gv1)
 }
